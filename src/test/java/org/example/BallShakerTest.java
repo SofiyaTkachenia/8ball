bb9594c20919aa33282eb6871267a487d18d9ec8
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2024-09-02 20:26:09 +0300

    fix: Fix the test

--- src/test/java/org/example/BallShakerTest.java
+++ src/test/java/org/example/BallShakerTest.java
@@ -3,6 +3,8 @@ package org.example;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.EnumSet;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BallShakerTest {
@@ -10,7 +12,8 @@ class BallShakerTest {
     private final BallShaker ballShaker = new BallShaker();
 
     @Test
-    void shakeTheBallReturnsValidAnswer() {
+    @DisplayName("Shake the ball returns valid answer")
+    void testCase01(){
         var possibleAnswers = EnumSet.allOf(Answer.class);
         var ballAnswer = ballShaker.shakeTheBall();
         assertTrue(possibleAnswers.contains(ballAnswer));
